eaf16717743709264402d13f8dabcbfb9fd21eb3
abdu11ahfa1zan <abdullahbinfaizan09@gmail.com>
2025-06-27 16:16:00 +0500

    Redid with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Pattern from "./patterns/Pattern"
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Pattern />
+    </>
+  )
+}
+
+export default App
